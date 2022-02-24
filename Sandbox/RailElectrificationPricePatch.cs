75d04197eaeba3f66ab7c814a43b57ad22b73847
Jorn Hertsig <jorn86@gmail.com>
2022-02-24 21:36:27 +0100

    Add rail electrification & release 0.1.1

---
+++ Sandbox/RailElectrificationPricePatch.cs
@@ -0,0 +1,15 @@
+﻿using HarmonyLib;
+using VoxelTycoon.Buildings;
+using VoxelTycoon.Tracks.Rails;
+
+namespace Sandbox
+{
+    [HarmonyPatch(typeof(Rail), nameof(Rail.ElectrificationPrice), MethodType.Getter)]
+    class RailElectrificationPricePatch
+    {
+        internal static bool Prefix(ref double __result)
+        {
+            return SandboxSettings.FreeIf(SandboxSettings.FreeInfra, ref __result);
+        }
+    }
+}
---
+++ Sandbox/changelog.md
@@ -0,0 +1,12 @@
+﻿# Changelog
+All notable changes to this project will be documented in this file.
+
+The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
+and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
+
+## [0.1.1] - 2022-02-24
+### Added
+- Rail elecrification, will be free if the Infrastructure setting is enabled.
+
+## [0.1.0] - 2022-02-23
+First release
