99f6ad18bd2d1ba7fb93135cb9fa97b6c58e358b
Jorn Hertsig <jorn86@gmail.com>
2022-03-02 12:44:25 +0100

    Add signal distance mod

---
+++ SignalDistance/SignalBuilderToolPatch.cs
@@ -0,0 +1,61 @@
+﻿using HarmonyLib;
+using VoxelTycoon;
+using VoxelTycoon.Tools;
+using VoxelTycoon.Tracks.Rails;
+using System;
+using VoxelTycoon.Tracks;
+using System.Collections.Generic;
+
+namespace SignalDistance
+{
+    [HarmonyPatch(typeof(RailSignalBuilderTool), methodName: "GetMessage")]
+    class SignalBuilderToolPatch
+    {
+        private static readonly Logger _logger = new Logger<SignalBuilderToolPatch>();
+
+        internal static void Postfix(RailConnection connection, ref string __result)
+        {
+            var forward = DistanceToNextSignal(connection, c => c.Signal);
+            var back = DistanceToNextSignal(connection.InnerConnection, c => c.InnerConnection.Signal);
+            
+            __result += $" - {forward} / {back}";
+        }
+
+        private static string DistanceToNextSignal(RailConnection connection, Func<RailConnection, RailSignal> signalSelector)
+        {
+            var next = connection;
+            var length = 0f;
+            var visited = new List<Track>();
+            while (true)
+            {
+                if (visited.Contains(next.Track))
+                {
+                    //_logger.Log("Next already visited");
+                    break;
+                }
+
+                if (next != connection && signalSelector.Invoke(next) != null)
+                {
+                    //_logger.Log("Next has signal");
+                    break;
+                }
+
+                visited.Add(next.Track);
+                length += next.Length;
+
+                next = next.InnerConnection;
+                if (next.OuterConnectionCount == 0) break;
+                if (next.OuterConnectionCount > 1)
+                {
+                    //_logger.Log("Next has " + next.OuterConnectionCount);
+                    return $"intersection {ToolHelper.FormatDistance(length)}";
+                }
+                else
+                {
+                    next = next.GetOuterConnection(0);
+                }
+            }
+            return ToolHelper.FormatDistance(length);
+        }
+    }
+}
---
+++ SignalDistance/SignalDistance.cs
@@ -0,0 +1,24 @@
+﻿using HarmonyLib;
+using VoxelTycoon;
+using VoxelTycoon.Modding;
+
+namespace SignalDistance
+{
+    public class SignalDistance : Mod
+    {
+        private static readonly Logger _logger = new Logger<SignalDistance>();
+        private const string PatchId = "org.hertsig.voxeltycoon.SignalDistance";
+
+        protected override void Initialize()
+        {
+            new Harmony(PatchId).PatchAll();
+            _logger.Log("SignalDistance patch complete");
+        }
+
+        protected override void Deinitialize()
+        {
+            new Harmony(PatchId).UnpatchAll();
+            _logger.Log("SignalDistance un-patch complete");
+        }
+    }
+}
