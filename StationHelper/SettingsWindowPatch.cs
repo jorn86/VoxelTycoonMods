6fda200a081f3cc31f9a2185ac40f201bfb62eb9
Jorn Hertsig <jorn86@gmail.com>
2022-03-27 16:28:44 +0200

    Add station helper mod (not released)

---
+++ StationHelper/SettingsWindowPatch.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using HarmonyLib;
+using UnityEngine.UI;
+using VoxelTycoon;
+using VoxelTycoon.Game.UI;
+using VoxelTycoon.Tools;
+
+namespace StationHelper
+{
+    [HarmonyPatch(typeof(SettingsWindowUpDownItem))]
+    class SettingPatch
+    {
+        private static readonly Logger _logger = new Logger<RailElectrificationTool>();
+        private static bool _inPatch;
+
+        [HarmonyPatch(nameof(SettingsWindowUpDownItem.Less))]
+        internal static void Postfix(SettingsWindowUpDownItem __instance, Text ___NameText)
+        {
+            Run(___NameText.text, __instance.Less);
+        }
+
+        [HarmonyPatch(nameof(SettingsWindowUpDownItem.More))]
+        internal static void Postfix(SettingsWindowUpDownItem __instance, Text ___NameText, object ___DescriptionText)
+        {
+            Run(___NameText.text, __instance.More);
+        }
+
+        private static void Run(string name, Action action)
+        {
+            if (!Active(name)) return;
+
+            _inPatch = true;
+            for (var i = 0; i < 9; i++)
+            {
+                action();
+            }
+            _inPatch = false;
+        }
+
+        private static bool Active(string name)
+        {
+            return !_inPatch && InputHelper.Control &&
+                   (name == S.RailStationBuilderWindowLength ||
+                    name == S.RailSignalBuilderToolSpacing);
+        }
+    }
+}
---
+++ StationHelper/StationHelper.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Linq;
+using HarmonyLib;
+using VoxelTycoon;
+using VoxelTycoon.Buildings;
+using VoxelTycoon.Modding;
+using VoxelTycoon.Tools;
+using VoxelTycoon.Tools.Builder;
+using VoxelTycoon.Tools.TrackBuilder;
+using VoxelTycoon.Tracks.Rails;
+using VoxelTycoon.UI;
+
+namespace StationHelper
+{
+    public class StationHelper : Mod
+    {
+        private static readonly Logger _logger = new Logger<StationHelper>();
+        private const string PatchId = "org.hertsig.voxeltycoon.StationHelper";
+
+        protected override void Initialize()
+        {
+            new Harmony(PatchId).PatchAll();
+            _logger.Log("StationHelper patch complete");
+        }
+
+        protected override void OnGameStarted()
+        {
+            new RailBuilderTool(null).Use(it =>
+            {
+                if (it.ElectrificationAvailable) it.ElectrificationMode = RailElectrificationMode.Right;
+            });
+
+            AssetLibrary.Current.GetAll<RailSignal>()
+                         .Select(railSignal => new RailSignalBuilderTool(railSignal))
+                         .ForEach(tool => tool.Use(it => it.Spacing = 30));
+
+            BuildingRecipeManager.Current.GetAll().Enumerate()
+                .OfType<RailStationRecipe>()
+                .Select(recipe => new RailStationBuilderTool(recipe.Building as RailStation))
+                .ForEach(tool => tool.Use(it => { it.Width = 2; it.Length = 30; }));
+        }
+
+        protected override void Deinitialize()
+        {
+            new Harmony(PatchId).UnpatchAll();
+            _logger.Log("StationHelper un-patch complete");
+        }
+    }
+
+    static class ToolExt
+    {
+        internal static void Use<T>(this T tool, Action<T> action) where T : ITool
+        {
+            tool.Activate();
+            action(tool);
+            if (!tool.Deactivate(true))
+            {
+                tool.Deactivate(false);
+            }
+        }
+    }
+}
